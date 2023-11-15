d513f515442ab701855c3cdeaf50a50b5866d8f7
G00395612@atu.ie <G00395612@atu.ie>
2023-11-15 09:13:00 +0000

    Initial Commit

---
+++ src/main/java/ie/atu/week7_validation/Week7ValidationApplication.java
@@ -0,0 +1,13 @@
+package ie.atu.week7_validation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Week7ValidationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Week7ValidationApplication.class, args);
+    }
+
+}
